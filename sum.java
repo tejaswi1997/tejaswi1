6023b376d499c8d1c228bd4d7823e1ebcfbffa8f
tejaswi1997 <41731930+tejaswi1997@users.noreply.github.com>
2018-07-27 14:40:31 +0530

    Create sum.java

---
+++ sum.java
@@ -0,0 +1,29 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int n,k,sum=0;
+		int a[]=new int[10];
+		Scanner s=new Scanner(System.in);
+		n=s.nextInt();
+		k=s.nextInt();
+		for(int i=0;i<n;i++)
+		{
+			a[i]=s.nextInt();
+			if(i<k)
+			{
+				sum=sum+a[i];
+				}
+				
+		}
+		System.out.println(sum);
+	}
+		}
+	
