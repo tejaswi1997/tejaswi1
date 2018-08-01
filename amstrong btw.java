7eed5be9538502aa52434a50b9f029890d9659b9
tejaswi1997 <41731930+tejaswi1997@users.noreply.github.com>
2018-08-01 14:00:31 +0530

    Create amstrong btw.java

---
+++ amstrong btw.java
@@ -0,0 +1,44 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int n,k,count=0,a;
+		Scanner s=new Scanner(System.in);
+		n=s.nextInt();
+		k=s.nextInt();
+		for(int i=n+1;i<k;i++)
+		{
+			int m=0;
+			int temp=i;
+			while(temp>0)
+			{
+				a=temp%10;
+				count=count+(a*a*a);
+				temp=temp/10;
+			}
+			if(count==i)
+			{
+				if(m<1)
+				{
+					System.out.println(i);
+				}else
+				{
+					System.out.print(" ");
+					System.out.print(i);
+					}
+					m++;
+				}
+				count=0;
+			}
+				
+		
+			
+		}
+	}
