34886340f57dcb140bf1e7f93d376f16e1d7e7cd
tejaswi1997 <41731930+tejaswi1997@users.noreply.github.com>
2018-08-03 14:11:19 +0530

    Create array.java

---
+++ array.java
@@ -0,0 +1,32 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int n, max;
+		Scanner s = new Scanner(System.in);
+		System.out.print("Enter number of elements in array:");
+		n =s.nextInt();	
+		int a[] = new int[n];
+		System.out.print("Enter elements of array:");
+		for(int i=0; i<n; i++)
+		{
+			a[i] = s.nextInt();
+		}
+		max = a[0];
+		for(int i=0; i<n; i++)
+		{
+			if(max < a[i])
+			{
+				max =a[i];
+			}
+		}
+		System.out.print(+max);
+	}
+}
