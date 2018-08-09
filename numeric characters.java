50b7a2b84fac58544a06d1494825033d25e9a4c6
tejaswi1997 <41731930+tejaswi1997@users.noreply.github.com>
2018-08-09 10:49:04 +0530

    Create numeric characters.java

---
+++ numeric characters.java
@@ -0,0 +1,27 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int count=0;
+		String st;
+		Scanner s=new Scanner(System.in);
+		st=s.nextLine();
+		int n=st.length();
+		for(int i=0;i<n;i++)
+		{
+			char c=st.charAt(i);
+			if(c>='0' && c<='9')
+			{
+				count++;
+			}
+		}
+		System.out.print(count);
+	}
+}
