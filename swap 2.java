0d24cdbe9437cec2b2f317ec713805ea08570177
tejaswi1997 <41731930+tejaswi1997@users.noreply.github.com>
2018-08-13 12:27:19 +0530

    Create swap 2.java

---
+++ swap 2.java
@@ -0,0 +1,21 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int a,b,temp;
+		Scanner sc=new Scanner(System.in);
+		a=sc.nextInt();
+		b=sc.nextInt();
+		temp=a;
+		a=b;
+		b=temp;
+		System.out.print(a+" "+b);
+}
+}
