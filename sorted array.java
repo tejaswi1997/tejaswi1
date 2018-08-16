9044c2ac5aa15d3066ee23ea121194e0a02b5776
tejaswi1997 <41731930+tejaswi1997@users.noreply.github.com>
2018-08-16 10:05:40 +0530

    Create sorted array.java

---
+++ sorted array.java
@@ -0,0 +1,38 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int n,i,j,temp;
+		int a[]=new int [20];
+		Scanner s=new Scanner(System.in);
+		n=s.nextInt();
+		for(i=0;i<n;i++)
+		{
+			a[i]=s.nextInt();
+		}
+		for(i=0;i<n;i++)
+		{
+			for(j=i+1;j<n;j++)
+			{
+				if(a[i]>a[j])
+				{
+					temp=a[i];
+					a[i]=a[j];
+					a[j]=temp;
+				}
+			}
+		}
+		for(i=0;i<n-1;i++)
+		{
+			System.out.print(a[i]+" ");
+		}
+		System.out.println(a[n-1]);
+	}
+}
