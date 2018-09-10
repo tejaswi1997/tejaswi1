38ec23777c5919550a1646c3f810fbbda8dc6726
tejaswi1997 <41731930+tejaswi1997@users.noreply.github.com>
2018-09-10 15:33:18 +0530

    Create 69.java

---
+++ 69.java
@@ -0,0 +1,23 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int num1=s.nextInt();
+		int num2=s.nextInt();
+		s.close();
+		int res=num1-num2;
+		if(res %2==0)
+		System.out.println("even");
+		else
+		System.out.println("odd");
+		
+	}
+}
